44f4eee2cc1ae1bdc9ed9f6e7f2808253decdea9
MajaZarkova <majazarkova@gmail.com>
2024-11-29 17:18:51 +0200

    Implement onyx

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import './assets/main.css'
+import 'sit-onyx/style.css'
+import '@fontsource-variable/source-code-pro'
+import '@fontsource-variable/source-sans-3'
 
 import { createApp } from 'vue'
+import { createOnyx } from 'sit-onyx'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const onyx = createOnyx()
 
 app.use(router)
 
 app.mount('#app')
+app.use(onyx)
